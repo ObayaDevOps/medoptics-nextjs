ad966247be90678beed2f74c6bc082872dc0c41d
ObayaDevOps <obayajamesdralega@gmail.com>
2023-03-21 17:33:14 +0100

    removed the use of CDN

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const client = sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "v2023-02-21",
-  useCdn: true
+  useCdn: false
   })
 
 
